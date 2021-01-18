8a93d1d8d76212c34efd4946e2d5c3f566604dd4
Frans <45152437+Fcr25@users.noreply.github.com>
2021-01-18 22:06:26 +0100

    init

---
+++ main.js
@@ -0,0 +1,23 @@
+const images = document.querySelectorAll('img')
+const slider = document.querySelector('.slider')
+
+let options = {
+    root: null,
+    rootMargin: '0px 100px 0px 0px',
+    threshold: 1
+}
+
+const slideObserver = new IntersectionObserver(callback = (entries, slideObserver) => {
+    entries.forEach(entry => {
+        console.log(entry)
+        if (!entry.isIntersecting) return
+        const image = entry.target
+        const newUrl = image.getAttribute('data-src')
+        image.src = newUrl
+        slideObserver.unobserve(image)
+    });
+}, options)
+
+images.forEach(image => {
+    slideObserver.observe(image)
+})
